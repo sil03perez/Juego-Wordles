ce71875cc41ff94eb0e6c50ed689ff8b3bb9fc10
Silvana <user@DESKTOP-EF46R3H>
2025-06-13 20:21:32 -0400

    parte del diseno en el wordles.tsx

--- src/components/wordle.tsx
+++ src/components/wordle.tsx
@@ -5,7 +5,7 @@ import RowEmpty from "./rowEmpty";
 import { GameStatus } from "./types";
 import { useWindow } from "../hooks/useWindows";
 import { getWordOfTheDay, isValidWord } from "../service/request";
-import { Style } from "./types";
+import style  from "./wordle.module.scss";
 
 const keys =  ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P",
                  "A", "S", "D", "F", "G", "H", "J", "K", "L",
